6f8c769ee9ebc6164fa12353bcef9834580ab124
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-07-22 13:13:27 +0900

    [Gold III] Title: 순회강연, Time: 228 ms, Memory: 19052 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -6,28 +6,41 @@ public class Main {
         return Integer.parseInt(s);
     }
 
+    private static class Pair implements Comparable<Pair> {
+        int p, d;
+
+        Pair(int p, int d) {
+            this.p = p;
+            this.d = d;
+        }
+
+        @Override
+        public int compareTo(Pair pair) {
+            return this.d - pair.d;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input;
 
         // 우선순위 최하를 맨 위에 나둔다.
         int n = pint(br.readLine());
-        int[][] arr = new int[n][2];
+        Pair[] arr = new Pair[n];
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.add(0);
 
         for (int i = 0; i < n; i++) {
             input = br.readLine().split(" ");
-            arr[i][0] = pint(input[0]);
-            arr[i][1] = pint(input[1]);
+            arr[i] = new Pair(pint(input[0]), pint(input[1]));
         }
 
-        Arrays.sort(arr, Comparator.comparingInt((int[] o) -> o[1]));
+        Arrays.sort(arr);
 
         int time = 1;
         for (int i = 0; i < n; i++) {
-            int p = arr[i][0];
-            int d = arr[i][1];
+            int p = arr[i].p;
+            int d = arr[i].d;
 
             if (d > time) { // 그냥 추가
                 time++;
