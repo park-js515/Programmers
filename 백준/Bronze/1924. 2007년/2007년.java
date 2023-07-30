067d67f6fea6da45aa7068bb532101c341645cbf
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-07-31 08:28:19 +0900

    [Bronze I] Title: 2007년, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -5,15 +5,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] days = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
-        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] months = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
         String[] input = br.readLine().split(" ");
 
         int x = Integer.parseInt(input[0]), y = Integer.parseInt(input[1]);
         int sum = y - 1;
-
-        for (int i = 0; i < x - 1; i++) {
-            sum += months[i];
-        }
+        sum += months[x - 1];
 
         System.out.println(days[sum % 7]);
         br.close();
