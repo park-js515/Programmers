010bb8dc8926d9eddae21fa87054399b21b5ad55
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-09-15 08:38:20 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -2,21 +2,9 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
 for i in range(1, N + 1):
-    for j in range(i):
-        print("*", end="")
-    for j in range(2 * (N - i)):
-        print(" ", end="")
-    for j in range(i):
-        print("*", end="")
-    print()
-
+    temp = "*" * i + " " *  (N - i)
+    print(temp + temp[::-1])
 for i in range(N - 1, 0, -1):
-    for j in range(i):
-        print("*", end="")
-    for j in range(2 * (N - i)):
-        print(" ", end="")
-    for j in range(i):
-        print("*", end="")
-    print()
+    temp = "*" * i + " " * (N - i)
+    print(temp + temp[::-1])
