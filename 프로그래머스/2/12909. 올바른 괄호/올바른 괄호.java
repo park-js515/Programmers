011cb210865a32014c8b20314c77e42e387bb40a
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-11-24 14:03:08 +0900

    [level 2] Title: 올바른 괄호, Time: 5.23 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean solution(String s) {
+        int open = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                open++;                
+            } else {
+                if (open > 0) {
+                    open--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        if (open > 0) return false;
+        return true;
+    }
+}
