8c95a18ead20c4cb6da11ceddc2ad3c1f993b3f9
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-11-27 21:24:47 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,23 @@
+class Solution {
+    private static int answer = 0;
+    private static boolean[] visited;
+    private static void dfs(int n, int k, int[][] dungeons, int depth) {
+        answer = Math.max(answer, depth);
+        
+        for (int i = 0; i < n; i++) {
+            int[] dungeon = dungeons[i];
+            if (!visited[i] && k >= dungeon[0]) {
+                visited[i] = true;
+                dfs(n, k - dungeon[1], dungeons, depth + 1);
+                visited[i] = false;
+            }    
+        }
+    }
+    
+    public int solution(int k, int[][] dungeons) {
+        int n = dungeons.length;
+        visited = new boolean[n];
+        dfs(n, k, dungeons, 0);
+        return answer;
+    }
+}
