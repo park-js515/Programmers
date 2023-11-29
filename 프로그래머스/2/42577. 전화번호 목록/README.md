86da5cf77a957d7878d5e97d7af243bd0fd8a063
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-11-29 11:43:29 +0900

    [level 2] Title: 전화번호 목록, Time: 201.68 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java#) 
 
 ### 성능 요약
 
-메모리: 133 MB, 시간: 237.68 ms
+메모리: 130 MB, 시간: 201.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 11:41:36
+2023년 11월 3일 11:43:27
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -6,10 +6,10 @@ class Solution {
         HashSet<String> set = new HashSet<>();
         Arrays.sort(phone_book, (o1, o2) -> {
             return o1.length() - o2.length();
-        });
+        }); // 접두사로 올 수 있는 것은 자신보다 짧은 길이이어야 하므로 오름차순으로 정렬합니다.
         for (String phone: phone_book) {
-            for (int i = 1; i <= phone.length(); i++) {
-                if (set.contains(phone.substring(0, i))) {
+            for (int i = 1; i <= phone.length(); i++) { 
+                if (set.contains(phone.substring(0, i))) { // String을 처음부터 끝까지 순회하면서 포함여부를 확인합니다.
                     return false;
                 }
             }
