5f32b51190bcbbb5cdb42c1b90c149469b3a6b68
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-12-06 23:59:26 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 14.30 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,27 +1,17 @@
-// 수학적으로 고심해야하는 문제
-// 좋은 문제인데, 생각하기에 너무 어렵다.
-// dfs에 빠져서 생각이 좀 많이 굳은 문제였던 거 같음.
-class Solution {    
+// 짝수 0으로 끝나므로 1만 더해주면 된다.
+// 홀수의 경우 01이 되는 경우를 10으로 바꿔주면 된다.
+class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
-                answer[i] = numbers[i] + 1;
-            } else {
-                String bin = Long.toString(numbers[i], 2);
-                long find0 = -1;
-                for (int j = bin.length() - 1; j >= 0; j--) {
-                    if (bin.charAt(j) == '0') {
-                        find0 = bin.length() - 1 - j;
-                        break;
-                    }
-                }
-                if (find0 == -1) {
-                    find0 = bin.length();
-                }
-                find0 = (long)Math.pow(2, find0);
-                answer[i] = numbers[i] + find0 - find0 / 2;
+            int cnt = 0;
+            long num = numbers[i];
+            while (num % 2 == 1) {
+                num /= 2;
+                cnt++;
             }
+            answer[i] = cnt == 0 ? numbers[i] + 1 : numbers[i] + (long)Math.pow(2, cnt - 1);
+            
         }
         return answer;
     }
