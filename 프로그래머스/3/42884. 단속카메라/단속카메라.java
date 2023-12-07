204483b95875a4265256adce5978920e16570ca7
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-12-07 15:14:34 +0900

    [level 3] Title: 단속카메라, Time: 7.02 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -11,7 +11,7 @@ class Solution {
                 return o1[1] - o2[1];
             }
         });
-        System.out.println(Arrays.deepToString(routes));
+        
         start = routes[0][0];
         end = routes[0][1];
         for (int i = 1; i < routes.length; i++) {
