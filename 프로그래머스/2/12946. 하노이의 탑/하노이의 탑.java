e4f09ee769c6d95667bfbe8fb08593ed5f0cdd45
park-js515 <122588631+park-js515@users.noreply.github.com>
2023-12-22 14:57:27 +0900

    [level 2] Title: 하노이의 탑, Time: 5.11 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    private ArrayList<int[]> list = new ArrayList<>();
+    private void hanoi(int n, int from, int to, int mid) {
+        if (n == 1) {
+            list.add(new int[] {from, to});
+            return;
+        }
+        hanoi(n - 1, from, mid, to);
+        list.add(new int[] {from, to});
+        hanoi(n - 1, mid, to, from);
+    }
+    
+    public int[][] solution(int n) {
+        hanoi(n, 1, 3, 2);
+        int[][] answer = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
