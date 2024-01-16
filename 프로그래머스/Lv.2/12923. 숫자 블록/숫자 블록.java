85bac6fef38e5bd19fc867dd7aae1c08e26c90f0
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-01-16 01:08:11 -0500

    [level Lv.2] Title: 숫자 블록, Time: 612.01 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12923. 숫자 블록/숫자 블록.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(long begin, long end) {
+        int len = (int) (end - begin + 1);
+        int[] answer = new int[len];
+        int M = 10_000_000;
+        int index = 0;
+        for (int i = (int) begin; i <= (int) end; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int j = 1; j <= (int) Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    list.add(j);
+                    list.add(i / j);
+                }
+            }
+            for (int j: list) {
+                if (j != i && j <= M) {
+                    answer[index] = Math.max(answer[index], j);
+                }
+            }
+            index++;
+        }
+        
+        if (begin == 1) {
+            answer[0] = 0;
+        }
+        
+        return answer;
+    }
+}
