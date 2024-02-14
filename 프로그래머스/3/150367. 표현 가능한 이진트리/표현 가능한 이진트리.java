039d6e6054de81905674765ecf04ecc93485a5db
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-02-14 05:54:23 -0500

    [level 3] Title: 표현 가능한 이진트리, Time: 12.64 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -0,0 +1,56 @@
+class Solution {
+    private int answer = 1;
+    private void check(String st, int start, int end, char before) {
+        if (answer == 0) {
+            return;
+        }
+        if (start == end) {
+            if (st.charAt(start) == '1' && before == '0') {
+                answer = 0;
+            }
+            return;
+        }
+        
+        int mid = (start + end) / 2;
+        char ch = st.charAt(mid);
+        if (ch == '1' && before == '0') {
+            answer = 0;
+            return;
+        }
+        check(st, start, mid - 1, ch);
+        check(st, mid + 1, end, ch);
+    }
+    
+    public int[] solution(long[] numbers) {
+        int len = numbers.length;
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            long number = numbers[i];
+            String st = Long.toBinaryString(number);
+            int stLen = st.length();
+            int k = 0;
+            while (Math.pow(2, k) - 1 < stLen) {
+                k++;
+            }
+            int targetLen = (int) Math.pow(2, k) - 1;
+            int gap = targetLen - stLen;
+            
+            
+            if (gap > 0) {
+                StringBuilder sb = new StringBuilder(st);
+                for (int j = 0; j < gap; j++) {
+                    sb.insert(0, "0");
+                }
+                answer = 1;
+                check(sb.toString(), 0, targetLen - 1, '1');
+                arr[i] = Math.max(arr[i], answer);
+            } else {
+                answer = 1;
+                check(st, 0, targetLen - 1, '1');
+                arr[i] = Math.max(arr[i], answer);
+            }
+        }
+        
+        return arr;
+    }
+}
