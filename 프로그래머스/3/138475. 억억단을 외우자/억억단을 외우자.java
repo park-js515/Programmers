aba7507c3b9b52dff24c6ff2de122a191534c6bc
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-03-11 10:15:13 -0400

    [level 3] Title: 억억단을 외우자, Time: 347.30 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int e, int[] starts) {
+        int[] arr = new int[e + 1];
+        for (int i = 1; i <= e; i++) {
+            for (int j = i; j <= e; j += i) {
+                arr[j]++;
+            }
+        }
+        
+        int[] maxArr = new int[e + 1];
+        int max = 0;
+        int index = -1;
+        for (int i = e; i >= 1; i--) {
+            if (max <= arr[i]) {
+                index = i;
+                max = arr[i];
+            }
+            maxArr[i] = index;
+        }
+        
+        int len = starts.length;
+        int[] answer = new int[len];
+        for (int i = 0; i < len; i++) {
+            answer[i] = maxArr[starts[i]];
+        }
+        
+        return answer;
+    }
+}
