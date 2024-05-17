ee25035780290dd3641a2db1eac51dbc5b8bfe1a
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-05-17 20:10:24 +0900

    [level 4] Title: 쌍둥이 빌딩 숲, Time: 1.88 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/4/140105. 쌍둥이 빌딩 숲/쌍둥이 빌딩 숲.java
+++ 프로그래머스/4/140105. 쌍둥이 빌딩 숲/쌍둥이 빌딩 숲.java
@@ -1,5 +1,7 @@
 // dp
 // 가장 작은 높이를 마지막에 놓는다고 생각하며 푼다.
+// 가장 작은 높이를 맨 앞에 놓으면 구분되는 빌딩(count)가 1 증가한다. -> 1개의 경우
+// 그 외의 경우에는 기존의 count에 해당하는 게 증가한다. -> (i - 1) * 2 개의 경우
 
 class Solution {
     private final int MOD = 1_000_000_007;
@@ -8,12 +10,10 @@ class Solution {
         long[][] dp = new long[n + 1][n + 1];
         dp[1][1] = 1;
         
-        long k = 0;
         for (int i = 2; i <= n; i++) {
-            k += 2;
             for (int j = 1; j <= i - 1; j++) {
                 dp[i][j + 1] += dp[i - 1][j];
-                dp[i][j] += k * dp[i - 1][j];
+                dp[i][j] += (i - 1) * 2 * dp[i - 1][j];
                 dp[i][j + 1] %= MOD;
                 dp[i][j] %= MOD;
             }
