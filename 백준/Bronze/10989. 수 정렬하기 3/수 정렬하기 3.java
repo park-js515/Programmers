153dc508c0cf1cfbbf50d47a33af78d27eba101b
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-06-04 01:52:15 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1752 ms, Memory: 297088 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        for (int i = 1; i < 10001; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+                bw.write(i + "\n");
+            }
+        }
+
+        bw.close();
+    }
+}
