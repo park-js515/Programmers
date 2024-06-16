da9cf742df14d56e72aa49f9d82c229809573a83
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-06-16 15:55:38 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -1,16 +1,14 @@
 class Solution {
     public int solution(int n) {
-        int[] dp1 = new int[n + 1];
-        int[] dp2 = new int[n + 1];
-        dp1[0] = 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
         
         for (int i = 1; i < n + 1; i++) {
-            dp2[i] = dp1[i - 1];
             for (int j = 0; j < i; j++) {
-                dp1[i] += dp1[j] * dp2[i - j];
+                dp[i] += dp[j] * dp[i - j - 1];
             }
         }
-
-        return dp1[n];
+        
+        return dp[n];
     }
 }
