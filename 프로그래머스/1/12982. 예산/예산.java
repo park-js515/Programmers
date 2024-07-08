a78f365b78e288d743b3ceff972a9ca9ae511ab1
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-07-08 13:12:45 +0900

    [level 1] Title: 예산, Time: 0.51 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        
+        int sum  = 0;
+        int answer = 0;
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            
+            if (sum <= budget) {
+                answer++;
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
