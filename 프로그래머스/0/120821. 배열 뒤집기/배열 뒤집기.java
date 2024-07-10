47be1a28a58ddc1f17aeb6215e35ad364d0e94ef
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-07-10 19:58:04 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.length;
+        int[] answer = new int[len];
+        for (int i = 0; i < len; i++) {
+            answer[len - i - 1] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
