aa47a760f9ec6c9b303fd71c9093bac421aab8b7
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-07-25 10:34:03 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.70 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char ch  = my_string.charAt(i);
+            
+            if (Character.isLowerCase(ch)) {
+                sb.append(Character.toUpperCase(ch));
+            } else {
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
