cd79ddcc835e28714453415653573bd2a3634ca1
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-08-26 13:49:23 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,7 +1,14 @@
-import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        return IntStream.rangeClosed(start_num, end_num).toArray();
+        int cnt = end_num - start_num + 1;
+        int index = 0;
+        int[] answer = new int[cnt];
+        
+        for (int i = 0; i < cnt; i++) {
+            answer[i] = start_num++;
+        }
+        
+        return answer;
     }
 }
