3e08ad5c02f488408ffb8dbada1cdc18ff397238
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-08-29 12:17:00 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.55 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,6 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        return Arrays.stream(num_list).sorted().limit(5).toArray();
+        // return Arrays.stream(num_list).sorted().limit(5).toArray();
+        Arrays.sort(num_list);
+        int[] answer = new int[5];
+        for (int i = 0; i < 5; i++) {
+            answer[i] = num_list[i];
+        }
+        
+        return answer;
     }
 }
