abdecb10b8d71de893fc50f787e5473e9f3a75f5
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-09-03 20:09:35 +0900

    [level 0] Title: ad 제거하기, Time: 2.18 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < strArr.length; i++) {
+            if (!strArr[i].contains("ad")) {
+                list.add(strArr[i]);
+            }
+        }
+        
+        return list.toArray(String[]::new);
+    }    
+}
