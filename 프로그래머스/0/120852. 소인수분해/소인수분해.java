934669df2d3743ba8a10bbe313ae02abe48a6052
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-09-24 16:51:42 +0900

    [level 0] Title: 소인수분해, Time: 7.56 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
-                set.add(i);
+                list.add(i);
                 while (n % i == 0) {
                     n /= i;
                 }
@@ -13,9 +13,7 @@ class Solution {
             
             if (n == 1) break;
         }
-        
-        List<Integer> list = new ArrayList<>(set);
-        Collections.sort(list);
+
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
