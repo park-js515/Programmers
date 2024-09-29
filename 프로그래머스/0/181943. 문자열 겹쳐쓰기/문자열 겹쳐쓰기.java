431bf0a7437a9d76db4036abca0d1fbdff9cf1f5
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-09-30 00:54:08 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.11 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder sb = new StringBuilder(my_string);
+        int len = overwrite_string.length();
+        sb.replace(s, s + len, overwrite_string);
+        
+        return sb.toString();
+    }
+}
