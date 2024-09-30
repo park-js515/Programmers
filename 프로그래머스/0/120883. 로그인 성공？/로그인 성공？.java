4de7dc02c10a64145331d63c528ae3202b0448c4
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-09-30 13:04:54 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        
+        for (String[] data : db) {
+            if (id_pw[0].equals(data[0])) {
+                if (id_pw[1].equals(data[1])) {
+                    answer = "login";
+                } else {
+                    answer = "wrong pw";
+                }
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
