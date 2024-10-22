b5b82f4782ed39be1aacff28d827d418dad96fce
park-js515 <122588631+park-js515@users.noreply.github.com>
2024-10-22 19:05:05 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 0.68 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int solution(int[] mats, String[][] park) {
+        int r = park.length;
+        int c = park[0].length;
+        int[][] dp = new int[r][c];
+        int max = 0;
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (park[i][j].equals("-1")) {
+                    dp[i][j] = 1;
+                    max = 1;    
+                };
+            }
+        }
+        
+        // for (int[] i : dp) {
+        //     System.out.println(java.util.Arrays.toString(i));
+        // }
+        System.out.println();
+        
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                if (dp[i][j] == 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        
+        int answer = -1;
+        for (int mat : mats) {
+            if (mat <= max) {
+                answer = Math.max(answer, mat);
+            }
+        }
+        
+        // for (int[] i : dp) {
+        //     System.out.println(java.util.Arrays.toString(i));
+        // }
+        // System.out.println(max);
+        
+        return answer;
+    }
+}
