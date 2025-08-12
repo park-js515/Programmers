cdc4bfcdea51d273e46e8fc6b3e204b045f56252
park-js515 <122588631+park-js515@users.noreply.github.com>
2025-08-13 02:02:56 +0900

    [level 2] Title: 올바른 괄호, Time: 15.75 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,19 +1,28 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
-        int open = 0;
+        ArrayDeque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                open++;                
+            char ch = s.charAt(i);
+            if (!stack.isEmpty() && ch == ')' && stack.peekLast() == '(') {
+                stack.pollLast();
             } else {
-                if (open > 0) {
-                    open--;
-                } else {
-                    return false;
-                }
+                stack.add(ch);
             }
         }
         
-        if (open > 0) return false;
-        return true;
+        while (stack.size() >= 2) {
+            char right = stack.pollLast();
+            char left = stack.pollLast();
+            
+            if (left == '(' && right == ')') {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        
+        return stack.size() == 0;
     }
 }
