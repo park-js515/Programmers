3be5b63d8c59a2eace4e65c6bcd95d420d548bcf
park-js515 <122588631+park-js515@users.noreply.github.com>
2025-08-17 17:34:58 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,35 @@
+import java.util.*;
+
 class Solution {
-    private static int find(int[] grp, int a) {
-        if (grp[a] == a) {
-            return a;
-        }
-        
-        grp[a] = find(grp, grp[a]);
-        return grp[a];
-    }
+    private boolean[] visited;
+    private int[][] computers;
     
-    private static void union(int[] grp, int a, int b) {
-        int x = find(grp, a);
-        int y = find(grp, b);
+    
+    private void bfs(int n, int node) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.add(node);
         
-        grp[y] = x;
+        while (!queue.isEmpty()) {
+            int i = queue.poll();
+            for (int j = 0; j < n; j++) {
+                if (computers[i][j] == 1 && !visited[j]) {
+                    visited[j] = true;
+                    queue.add(j);
+                }
+            }
+        }
     }
     
     public int solution(int n, int[][] computers) {
-        int[] grp = new int[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            grp[i] = i;
-        }
+        this.visited = new boolean[n];
+        this.computers = computers;
         
-        int answer = n;
+        int answer = 0;
         for (int i = 0; i < n; i++) {
-            int[] links = computers[i];
-            for (int j = i + 1; j < n; j++) {
-                if (links[j] == 1 && find(grp, i) != find(grp, j)) {
-                    answer--;
-                    union(grp, i, j);
-                }
+            if (!visited[i]) {
+                visited[i] = true;
+                answer++;
+                bfs(n, i);
             }
         }
         
