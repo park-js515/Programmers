badd2781d2ae41201463eb68f67f6811ce35c4f2
park-js515 <122588631+park-js515@users.noreply.github.com>
2025-08-19 18:10:07 +0900

    [level 3] Title: 아이템 줍기, Time: 11.44 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,92 +1,84 @@
-import java.util.ArrayDeque;
+import java.util.*;
 
 class Solution {
     private int[][] d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private boolean[][] toggle = new boolean[102][102];
+    private boolean[][] move = new boolean[102][102];
+    private int[][] dist = new int[102][102];
     
-    private int bfs(int[][][] road, int rs, int cs, int re, int ce) {
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[] {rs, cs});
-        int[][] visited = new int[52][52];
-        for (int i = 0; i < 52; i++) {
-            for (int j = 0; j < 52; j++) {
-                visited[i][j] = Integer.MAX_VALUE;
+    private void init(int[][] rectangle) {
+        for (int[] rect : rectangle) {
+            for (int i = rect[0] * 2; i <= rect[2] * 2; i++) {
+                for (int j = rect[1] * 2; j <= rect[3] * 2; j++) {
+                    toggle[i][j] = true;
+                }
             }
+
         }
-        visited[rs][cs] = 0;
         
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int r = now[0];
-            int c = now[1];
-            for (int i = 0; i < 4; i++) {
-                int dr = r + d[i][0];
-                int dc = c + d[i][1];
+        for (int[] rect : rectangle) {
+            for (int i = rect[0] * 2 + 1; i <= rect[2] * 2 - 1; i++) {
+                for (int j = rect[1] * 2 + 1; j <= rect[3] * 2 - 1; j++) {
+                    toggle[i][j] = false;
+                }
+            }
+        }       
+        
+        for (int i = 1; i < 102; i++) {
+            for (int j = 1; j < 102; j++) {
+                dist[i][j] = 1_000_000;
                 
-                if (road[r][c][i] > 0 && visited[r][c] + 1 < visited[dr][dc]) {
-                    visited[dr][dc] = visited[r][c] + 1;
-                    queue.add(new int[] {dr, dc});
+                if (!toggle[i][j]) continue;
+                
+                int cnt = 0;
+                for (int a = 0; a < 4; a++) {
+                    int dr = i + d[a][0];
+                    int dc = j + d[a][1];
+                    if (toggle[dr][dc]) cnt++;
+                }
+                
+                if (cnt == 2) {
+                    move[i][j] = true;
                 }
             }
         }
+    }
+    
+    private int bfs(int rs, int cs, int re, int ce) {
+        ArrayDeque<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(rs, cs));
+        System.out.println(rs + " " + cs);
+        dist[rs][cs] = 0;
         
-        return visited[re][ce];
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            
+            for (int i = 0; i < 4; i++) {
+                int dr = point.r + d[i][0];
+                int dc = point.c + d[i][1];
+                
+                if (move[dr][dc] && dist[point.r][point.c] + 1 < dist[dr][dc]) {
+                    dist[dr][dc] = dist[point.r][point.c] + 1;
+                    queue.add(new Point(dr, dc));
+                }
+            }
+        }
+
+        return dist[re][ce] / 2;
     }
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        int[][][] road = new int[52][52][4];
-        for (int x = 0; x < rectangle.length; x++) {
-            int[] r = rectangle[x];
-            int X = x + 1;
-            int rs = r[1], cs = r[0], re = r[3], ce = r[2];
-            for (int i = rs; i < re; i++) {
-                if (road[i][cs][1] != -1) {
-                    road[i][cs][1] = X;
-                }
-                if (road[i][ce][1] != -1) {
-                    road[i][ce][1] = X;
-                }
-            }
-            for (int i = re; i > rs; i--) {
-                if (road[i][cs][0] != -1) {
-                    road[i][cs][0] = X;
-                }
-                if (road[i][ce][0] != -1) {
-                    road[i][ce][0] = X;
-                }
-            }
-            for (int i = cs; i < ce; i++) {
-                if (road[rs][i][3] != -1) {
-                    road[rs][i][3] = X;
-                }
-                if (road[re][i][3] != -1) {
-                    road[re][i][3] = X;
-                }
-            }
-            for (int i = ce; i > cs; i--) {
-                if (road[rs][i][2] != -1) {
-                    road[rs][i][2] = X;
-                }
-                if (road[re][i][2] != -1) {
-                    road[re][i][2] = X;
-                }
-            }
-            
-            for (int i = rs; i <= re; i++) {
-                for (int j = cs; j <= ce; j++) {
-                    for (int k = 0; k < 4; k++) {
-                        int dr = i + d[k][0];
-                        int dc = j + d[k][1];
-                        if (dr < rs || dr > re || dc < cs || dc > ce) {
-                            continue;
-                        }
-                        if (road[i][j][k] != X) {
-                            road[i][j][k] = -1;
-                        }
-                    }
-                }
-            }
-        }
+        init(rectangle);
         
-        return bfs(road, characterY, characterX, itemY, itemX);
+        return bfs(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
+    }
+    
+    private class Point {
+        int r, c;
+        
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
     }
 }
