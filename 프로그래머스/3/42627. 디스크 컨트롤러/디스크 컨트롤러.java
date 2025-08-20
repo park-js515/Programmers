972ab304318a52d92a8b15e900d1885511d47c1b
park-js515 <122588631+park-js515@users.noreply.github.com>
2025-08-20 14:12:41 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 5.08 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,33 +1,83 @@
-// 한국어가 너무 어려운 문제
-import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Comparator;
 
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, Comparator.comparingInt(o -> o[0]));
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        int n = jobs.length;
+        int time = 0;
+        int sum = 0;
+        PriorityQueue<Disk> disks = new PriorityQueue<>();
+        PriorityQueue<Task> tasks = new PriorityQueue<>();
         
-        int len = jobs.length;
-        int total = 0;
-        int now = 0;
-        int idx = 0;
-        int cnt = 0;
-        while (cnt < len) {
-            while (idx < len && jobs[idx][0] <= now) {
-                pq.add(jobs[idx++]);
+        for (int i = 0; i < n; i++) {
+            disks.add(new Disk(i, jobs[i][0], jobs[i][1]));
+        }
+        
+        while (!disks.isEmpty() || !tasks.isEmpty()) {
+            // tasks 작업 처리 후 시간 계산
+            if (!tasks.isEmpty()) {
+                Task task = tasks.poll();
+                time += task.c;
+                sum += time - task.b;
             }
-
-            if (pq.isEmpty()) {
-                now = jobs[idx][0];
-            } else {
-                int[] task = pq.poll();
-                total += now - task[0] + task[1];
-                now += task[1];
-                cnt++;
+            
+            // disks에서 빼서 넣기
+            while (!disks.isEmpty() && disks.peek().b <= time) {
+                tasks.add(new Task(disks.poll()));
+            }
+            
+            if (tasks.isEmpty() && !disks.isEmpty()) {
+                Disk disk = disks.poll();
+                time = disk.b;
+                tasks.add(new Task(disk));
+                
+                while (!disks.isEmpty() && disks.peek().b == time) {
+                    tasks.add(new Task(disks.poll()));
+                }
             }
         }
         
-        return total / jobs.length;
+        return sum / n;
+    }
+    
+    private class Disk implements Comparable<Disk> {
+        int a, b, c;
+        
+        public Disk(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        
+        @Override
+        public int compareTo(Disk o) {
+            return Integer.compare(this.b, o.b);
+        }
+    }
+    
+    private class Task implements Comparable<Task> {
+        int a, b, c; // 작업 번호, 작업 요청 시간, 작업 소요 시간
+        
+        public Task(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        
+        public Task(Disk disk) {
+            this.a = disk.a;
+            this.b = disk.b;
+            this.c = disk.c;
+        }
+        
+        @Override
+        public int compareTo(Task o) {
+            if (this.c != o.c) {
+                return Integer.compare(this.c, o.c);
+            }
+            if (this.b != o.b) {
+                return Integer.compare(this.b, o.b);
+            }
+            return Integer.compare(this.a, o.a);
+        }
     }
 }
