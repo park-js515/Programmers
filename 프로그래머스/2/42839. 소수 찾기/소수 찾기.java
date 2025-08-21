b3ed9de2116244806d49d343181b190a89bc705f
park-js515 <122588631+park-js515@users.noreply.github.com>
2025-08-21 15:41:55 +0900

    [level 2] Title: 소수 찾기, Time: 83.03 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,40 +1,39 @@
 class Solution {
-    private static int answer = 0;
-    private static boolean[] visited = new boolean[10000000];
-    private static boolean[] primeVisited = new boolean[10000000];
-    private static boolean[] primeArr = getPrimeArr(10000000);
-    private static boolean[] getPrimeArr(int N) {
-        boolean[] arr = new boolean[N + 1];
-        arr[0] = true;
-        arr[1] = true;
-        for (int i = 2; i <= (int)Math.sqrt(N) + 1; i++) {
-            if (!arr[i]) {
-                for (int j = i * 2; j < N + 1; j += i) {
-                    arr[j] = true;
-                }
+    boolean[] isNotPrime = new boolean[10_000_001];
+    private boolean[] check = new boolean[10_000_001];
+    private int answer = 0;
+    
+    private void init() {
+        isNotPrime[0] = isNotPrime[1] = true;
+        for (int i = 2; i <= (int) Math.sqrt(10_000_001); i++) {
+            if (isNotPrime[i]) continue;
+            for (int j = i * 2; j <= 10_000_000; j += i) {
+                isNotPrime[j] = true;
             }
         }
-        
-        return arr;
     }
-    private static void dfs(int N, String numbers, StringBuilder sb) {
-        for (int i = 0; i < N; i++) {
+    
+    private void dfs(String numbers, int num, int depth, int n, boolean[] visited) {
+        if (!isNotPrime[num] && !check[num]) {
+            check[num] = true;
+            answer++;
+        }
+        
+        int pow = (int) Math.pow(10, depth);
+        for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                StringBuilder nextSb = new StringBuilder(sb);
-                nextSb.append(numbers.charAt(i));
-                int val = Integer.parseInt(nextSb.toString());
-                if (!primeArr[val] && !primeVisited[val]) {
-                    primeVisited[val] = true;
-                    answer++;
-                }
-                dfs(N, numbers, nextSb);
+                int d = pow * (numbers.charAt(i) - '0');
+                dfs(numbers, num + d, depth + 1, n, visited);
                 visited[i] = false;
             }
         }
     }
+    
     public int solution(String numbers) {
-        dfs(numbers.length(), numbers, new StringBuilder());
+        init();
+        dfs(numbers, 0, 0, numbers.length(), new boolean[numbers.length()]);
+        
         return answer;
     }
 }
