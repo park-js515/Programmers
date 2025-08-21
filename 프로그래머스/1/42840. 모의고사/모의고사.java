8b869e80f2da084581391ca6b6f07e2b56087641
park-js515 <122588631+park-js515@users.noreply.github.com>
2025-08-21 13:45:36 +0900

    [level 1] Title: 모의고사, Time: 3.55 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,31 +1,33 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int len = answers.length;
         int[][] patterns = {
             {1, 2, 3, 4, 5},
             {2, 1, 2, 3, 2, 4, 2, 5},
             {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
         };
-        int[] pLen = {5, 8, 10};
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] c = new int[3];
         
-        int sum = 0;
         for (int i = 0; i < 3; i++) {
-            int temp = 0;
-            for (int j = 0; j < len; j++) {
-                temp += answers[j] == patterns[i][j % pLen[i]] ? 1: 0;
-            }
-            if (temp > sum) {
-                sum = temp;
-                list.clear();
-                list.add(i + 1);
-            } else if (temp == sum) {
-                list.add(i + 1);
+            for (int j = 0; j < answers.length; j++) {
+                int answer = answers[j];
+                c[i] += patterns[i][j % patterns[i].length] == answer ? 1 : 0;
             }
         }
         
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        int max = 0;
+        for (int i = 0; i < 3; i++) {
+            max = Math.max(max, c[i]);
+        }
+        
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (c[i] == max) {
+                answer.add(i + 1);
+            }
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
