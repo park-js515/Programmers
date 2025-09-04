292fed3c4945c8c0cb61be5607563de2c7101f96
park-js515 <122588631+park-js515@users.noreply.github.com>
2025-09-04 17:45:09 +0900

    [level 1] Title: 체육복, Time: 0.50 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,31 +2,40 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        boolean[] r = new boolean[n + 2];
+        boolean[] losted = new boolean[n + 2];
+        for (int i : reserve) {
+            r[i] = true;
+        }
+        for (int i : lost) {
+            losted[i] = true;
+        }
         Arrays.sort(lost);
-        Arrays.sort(reserve);
+        
         
         int answer = n - lost.length;
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
-                }
+        for (int i : lost) {
+            if (losted[i] && r[i]) {
+                losted[i] = false;
+                r[i] = false;
+                answer++;
             }
         }
         
-        for (int i = 0; i < lost.length; i++) {
-            if (lost[i] == -1) continue;
-            for (int j = 0; j < reserve.length; j++) {
-                if (reserve[j] == -1) continue;
-                if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-                    lost[i] = -1;
-                    reserve[j] = - 1;
-                    answer++;
-                    break;
-                }
+        for (int i : lost) {
+            if (!losted[i]) {
+                continue;
+            }
+            losted[i] = false;
+            
+            if (r[i - 1]) {
+                r[i - 1] = false;
+                answer++;
+                continue;
+            }
+            if (r[i + 1]) {
+                r[i + 1] = false;
+                answer++;
             }
         }
         
